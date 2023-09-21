a26018c9301ebeb248b4baa00e4ee04dc4c796d2
rainya512 <113275566+rainya512@users.noreply.github.com>
2023-09-21 14:29:19 +0900

    Create 06_challenge_1_copy_20230921142834.ino
    0921 archivement in progress

---
+++ 06_challenge_1_copy_20230921142834/06_challenge_1_copy_20230921142834.ino
@@ -0,0 +1,19 @@
+#define PIN_LED 7
+
+int set_period(int period) {
+
+}
+
+int set_duty(int duty) {
+  
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
